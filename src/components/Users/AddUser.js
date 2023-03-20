862db789185b21ae7f5966f4296071c78556ee84
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-20 13:48:38 -0300

    Adding User and Button

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from '../UI/Card';
+import Button from '../UI/Button';
+import classes from './AddUser.module.css';
+
+const AddUser = props => {
+    const addUserHandler = event => {
+        event.preventDefault();
+    };
+
+    return (
+        <Card cssClass={classes.input}>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Usernames</label>
+                <input id="username" type="text" />
+                <label htmlFor="age">Age (Years)</label>
+                <input id="age" type="number" />
+                <Button type="submit">Add User</Button>
+            </form>
+        </Card>
+    );
+};
+
+export default AddUser;

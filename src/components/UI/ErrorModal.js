0f086d06f38125274ba9d4dd45db69172a81b950
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-21 11:36:28 -0300

    Working with Portals
    Fragments and Portals helps write cleaner HTML code and we end up with semantically more correct HTML code. It makes the app more accessible without rendering unnecessarily many divs and in general shows that I am a developer.

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,23 +1,37 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Card from './Card'; // Using this component in more than 1 place in this project
 import Button from './Button'; // Using this component in more than 1 place in this project
 import classes from './ErrorModal.module.css';
 
+const Backdrop = props => {
+    return <div className={classes.backdrop} onClick={props.onConfirm} />;
+};
+
+const ModalOverlay = props => {
+    return (
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={classes.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={classes.actions}>
+                <Button onClick={props.onConfirm}>Okay</Button>
+            </footer>
+        </Card>
+    );
+};
+
 const ErrorModal = props => {
     return (
         <React.Fragment>
-            <div onClick={props.onConfirm} />
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={props.onConfirm}>Okay</Button>
-                </footer>
-            </Card>
+            {ReactDOM.createPortal(<Backdrop onConfirm={props.onConfirm} />, document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(
+                <ModalOverlay title={props.title} message={props.message} onConfirm={props.onConfirm} />,
+                document.getElementById('overlay-root')
+            )}
         </React.Fragment>
     );
 };
